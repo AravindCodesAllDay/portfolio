8e986af352cdc16588e49c5839cb15be6e0cfeb9
AravindCodesAllDay <aravindsiva1509@gmail.com>
2024-12-18 00:29:50 +0530

    removed error

--- app/error/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ThrowErrorPage() {
-  throw new Error("This is a deliberate error!");
-
-  return (
-    <div className="flex items-center justify-center h-screen text-lg font-semibold">
-      ThrowErrorPage
-    </div>
-  );
-}
